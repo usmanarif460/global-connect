c3256d73be434a95e4c60507876f90d522cb72c7
usmanarif460 <usmanarif460@gmail.com>
2025-04-05 17:10:58 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex bg-[#D6E8EB] p-12 flex-col sm:flex-row gap-5 sm:gap-0  h-screen  items-start  pt-36 justify-start sm:justify-center ">
-      <div className="flex items-center justify-center gap-2 fixed top-8 left-12">
+      <div className="flex items-center z-50 justify-center gap-2 absolute top-8 left-12">
         <Image
           src="/logo.svg"
           alt="Next.js logo"
@@ -14,7 +14,7 @@ export default function Home() {
         <h1 className="text-neutral-500 font-semibold">GlobalConnect</h1>
       </div>
 
-      <div className="flex w-full sm:basis-2/4 flex-col gap-6 items-center sm:items-start justify-center sm:pt-20">
+      <div className="flex w-full sm:basis-2/4 flex-col gap-6 items-center sm:items-start justify-center sm:pt-12">
         <h1 className="text-[60px] leading-16 font-semibold text-neutral-500">
           Launching Soon
         </h1>
@@ -24,7 +24,7 @@ export default function Home() {
           voluptatum autem quisquam impedit odio doloribus quibusdam magnam,
           sequi rerum enim.
         </p>
-        <button className="bg-[#00539B] rounded-lg text-white text-2xl px-8 py-3 font-medium">
+        <button className="bg-[#00539B] rounded-lg text-white text-lg sm:text-2xl sm:px-8 px-5 py-2 sm:py-3 font-medium">
           Contact Us
         </button>
       </div>
